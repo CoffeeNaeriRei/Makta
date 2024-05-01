fa0d75211d147722ef33dd5aa1184a555059133a
kybeen <rei980627@gmail.com>
2024-05-01 17:07:55 +0900

    [test] #10 - LocationServiceTests 생성 - LocationService의 테스트코드를 작성하는 클래스

---
+++ Projects/Data/Tests/LocationServiceTests.swift
@@ -0,0 +1,13 @@
+//
+//  LocationServiceTests.swift
+//  Data
+//
+//  Created by 김영빈 on 5/1/24.
+//
+
+import XCTest
+@testable import Data
+
+final class LocationServiceTests: XCTestCase {
+    
+}
