e1ae433be1400e971f837c025a283f64445bd771
kybeen <rei980627@gmail.com>
2024-05-02 19:36:34 +0900

    [feat] #10 - LocationService 동작 구현 - 위치서비스 승인 요청 처리 구현 - 현재 위치 불러오기 구현

---
+++ Projects/Data/Sources/External/LocationService/LocationService.swift
@@ -0,0 +1,83 @@
+//
+//  LocationService.swift
+//  Data
+//
+//  Created by 김영빈 on 5/1/24.
+//
+
+import CoreLocation
+
+enum LocationError: Error {
+    case fetchFailed
+}
+
+// MARK: - LocationService 정의
+
+final class LocationService: NSObject {
+    
+    typealias LocationCallback = (CLLocation?, Error?) -> Void // 위치 값을 인자로 전달하는 콜백 타입
+    
+    var locationManager: LocationManagerInterface
+    private var fetchLocationCallBack: LocationCallback?
+    
+    init(locationManager: LocationManagerInterface = CLLocationManager()) {
+        self.locationManager = locationManager
+        super.init()
+        self.locationManager.locationManagerDelegate = self
+    }
+    
+    // 현재 위치를 불러오기
+    func fetchCurrentLocation(completion: @escaping LocationCallback) {
+        fetchLocationCallBack = completion
+        locationManager.requestLocation()
+    }
+}
+
+// MARK: - LocationManagerDelegate 델리게이트 구현
+
+extension LocationService: LocationManagerDelegate {
+    
+    func locationManagerAbstract(_ manager: LocationManagerInterface, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse:
+            print("위치 서비스 사용 가능")
+        case .restricted, .denied:
+            print("위치 서비스 사용 불가")
+            // TODO: - "위치 서비스를 사용하려면 승인이 필요합니다." 팝업 띄우기
+        case .notDetermined:
+            print("권한 설정 필요")
+            locationManager.requestWhenInUseAuthorization() // 권한 요청
+        default:
+            break
+        }
+    }
+
+    func locationManagerAbstract(_ manager: LocationManagerInterface, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        fetchLocationCallBack?(location, nil)
+        fetchLocationCallBack = nil
+    }
+
+    func locationManagerAbstract(_ manager: LocationManagerInterface, didFailWithError error: Error) {
+        fetchLocationCallBack?(nil, LocationError.fetchFailed)
+        fetchLocationCallBack = nil
+        // TODO: - 실패 시 UI처리 필요
+    }
+}
+
+// MARK: - CLLocationManagerDelegate 델리게이트 구현
+
+extension LocationService: CLLocationManagerDelegate {
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        locationManagerAbstract(manager, didChangeAuthorization: manager.authorizationStatus)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManagerAbstract(manager, didUpdateLocations: locations)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManagerAbstract(manager, didFailWithError: error)
+    }
+}
