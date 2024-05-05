6a8e77f1a474e68f7b3576a72bf76e5357933cf3
yuncoffee <dong072815@gmail.com>
2024-05-05 21:51:33 +0900

    [chore] 임시코드 주석 추가 #6

--- Makcha/Makcha/Sources/Utils/UIFont+Extension.swift
+++ Makcha/Makcha/Sources/Utils/UIFont+Extension.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 
+// MARK: - 테스트용 임시 lineSpacing 변경 코드
 func setLineSpacing(_ spacing: CGFloat, text: String) -> NSAttributedString {
   let paragraphStyle = NSMutableParagraphStyle()
   paragraphStyle.lineSpacing = spacing
