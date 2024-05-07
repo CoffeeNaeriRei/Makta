d92288ade5cdf8659b04fd8b7d9e06f679217151
kybeen <rei980627@gmail.com>
2024-05-07 21:17:11 +0900

    [test] #20 - RxTest 프레임워크 추가

--- Makcha/MakchaTests/Data/Repository/TransPathRepositoryTests.swift
+++ Makcha/MakchaTests/Data/Repository/TransPathRepositoryTests.swift
@@ -5,4 +5,21 @@
 //  Created by 김영빈 on 5/7/24.
 //
 
-import Foundation
+import XCTest
+@testable import Makcha
+
+import RxSwift
+import RxTest
+
+final class TransPathRepositoryTests: XCTestCase {
+    private var sut: TransPathRepository!
+    private var disposeBag: DisposeBag!
+    
+    override func setUpWithError() throws {
+        super.setUp()
+    }
+    
+    override func tearDownWithError() throws {
+        super.tearDown()
+    }
+}
