bc2eb65f72d5f2a79617e209dbbd0f6b8e2a4021
kybeen <rei980627@gmail.com>
2024-05-07 15:26:39 +0900

    [feat] #20 - Domain 계층 인터페이스, 엔티티 파일 생성 - TransPathRepositoryProtocol - MakchPathSummary, MakchaPathDetail

---
+++ Makcha/Makcha/Sources/Domain/Entity/MakchaPathDetail.swift
@@ -0,0 +1,13 @@
+//
+//  MakchaPathDetail.swift
+//  Makcha
+//
+//  Created by 김영빈 on 5/7/24.
+//
+
+// MARK: - MakchaPathDetail
+// 각각의 막차 경로의 상세 정보를 담은 Entity 모델
+
+struct MakchaPathDetail {
+    
+}
