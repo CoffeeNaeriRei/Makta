748c7d16b40231445b4d00c0ce2b0b3a82512513
kybeen <rei980627@gmail.com>
2024-05-08 21:54:34 +0900

    [refactor] #20 - TransPathRepository 리팩토링 - 테스트 코드 작성을 위해 APIService 인터페이스 정의 후 의존성 제거 - DTO -> Entity 변환 메서드 분리

--- Makcha/Makcha/Sources/Data/External/APIService/APIService.swift
+++ Makcha/Makcha/Sources/Data/External/APIService/APIService.swift
@@ -10,37 +10,66 @@ import Foundation
 // MARK: - APIService 정의
 // API 호출 동작을 수행하는 객체
 
-struct APIService {
+protocol APIServiceInterface {
     
-    static func fetchTransPathData(
+    func fetchTransPathData(
         start: XYCoordinate,
         end: XYCoordinate,
-        resultSortType: ResultOrderType = .recommendPath,
-        searchType: SearchType = .inCity,
-        searchPathType: SearchPathType = .all,
-        completion: @escaping (Result<Data, APIServiceError>) -> Void) {
-            guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ODSAY_API_KEY") as? String else { return }
-            var transPathURL = "https://api.odsay.com/v1/api/searchPubTransPathT?"
-            transPathURL += "apiKey=\(apiKey)"
-            transPathURL += "&output=json"
-            transPathURL += "&SX=\(start.lonX)&SY=\(start.latY)"
-            transPathURL += "&EX=\(end.lonX)&EY=\(end.latY)"
-            transPathURL += "&OPT=\(resultSortType.rawValue)"
-            transPathURL += "&SearchType=\(searchType.rawValue)"
-            transPathURL += "&SearchPathType=\(searchPathType.rawValue)"
-            guard let url = URL(string: transPathURL) else {
+        completion: @escaping (Result<TransPathDTO, APIServiceError>) -> Void
+    )
+}
+
+struct APIService: APIServiceInterface {
+    
+    // API 요청
+    func fetchTransPathData(
+        start: XYCoordinate,
+        end: XYCoordinate,
+        completion: @escaping (Result<TransPathDTO, APIServiceError>) -> Void) {
+            guard let transPathURL = makeTransPathURL(start: start, end: end),
+                  let url = URL(string: transPathURL) else {
                 completion(.failure(APIServiceError.invalidURL))
                 return
             }
-            URLSession.shared.dataTask(with: url) { (data, response, error) in
-                if let _ = error {
+            
+            URLSession.shared.dataTask(with: url) { (data, _, error) in
+                if error != nil {
                     completion(.failure(APIServiceError.requestFail))
                 }
                 guard let data = data else {
                     completion(.failure(APIServiceError.noData))
                     return
                 }
-                completion(.success(data))
+                guard let transPathDTO = try? JSONDecoder().decode(TransPathDTO.self, from: data) else {
+                    completion(.failure(APIServiceError.decodingError))
+                    return
+                }
+                completion(.success(transPathDTO))
             }.resume()
+    }
+}
+
+extension APIService {
+    
+    // API 요청 URL 생성
+    func makeTransPathURL(
+        start: XYCoordinate,
+        end: XYCoordinate,
+        resultSortType: ResultOrderType = .recommendPath,
+        searchType: SearchType = .inCity,
+        searchPathType: SearchPathType = .all
+    ) -> String? {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ODSAY_API_KEY") as? String else {
+            return nil
+        }
+        var transPathURL = "https://api.odsay.com/v1/api/searchPubTransPathT?"
+        transPathURL += "apiKey=\(apiKey)"
+        transPathURL += "&output=json"
+        transPathURL += "&SX=\(start.lonX)&SY=\(start.latY)"
+        transPathURL += "&EX=\(end.lonX)&EY=\(end.latY)"
+        transPathURL += "&OPT=\(resultSortType.rawValue)"
+        transPathURL += "&SearchType=\(searchType.rawValue)"
+        transPathURL += "&SearchPathType=\(searchPathType.rawValue)"
+        return transPathURL
     }
 }
--- Makcha/Makcha/Sources/Data/Repository/TransPathRepository.swift
+++ Makcha/Makcha/Sources/Data/Repository/TransPathRepository.swift
@@ -14,19 +14,23 @@ import RxSwift
 
 final class TransPathRepository: TransPathRepositoryProtocol {
     
+    private let apiService: APIServiceInterface
+    
+    init(apiService: APIServiceInterface) {
+        self.apiService = apiService
+    }
+    
     // 대중교통 환승경로 정보를 받아와서 Domain 계층의 UseCase에 전달
-    func getAllMakchaTransPath(start: XYCoordinate, end: XYCoordinate) -> Observable<MakchaInfo> {
-        return Observable.create() { emitter in
-            APIService.fetchTransPathData(start: start, end: end) { [weak self] result in
+    func getAllMakchaTransPath(
+        start: XYCoordinate,
+        end: XYCoordinate
+    ) -> Observable<MakchaInfo> {
+        return Observable.create {emitter in
+            self.apiService.fetchTransPathData(start: start, end: end) { result in
                 switch result {
-                case .success(let data):
+                case .success(let transPathDTO):
                     print("[APIService] - ✅ fetchTransPathData() 호출 성공!!")
-                    guard let transPathDTO = try? JSONDecoder().decode(TransPathDTO.self, from: data) else {
-                        print("[APIService] - ❌ DTO 디코딩 실패")
-                        emitter.onError(APIServiceError.decodingError)
-                        return
-                    }
-                    guard let makchaInfo = self?.convertTransPathDTOToMakchaInfo(transPathDTO: transPathDTO) else {
+                    guard let makchaInfo = self.convertTransPathDTOToMakchaInfo(transPathDTO: transPathDTO) else {
                         print("[APIService] - ❌ DTO → Entity 변환 실패")
                         emitter.onError(APIServiceError.entityConvertError)
                         return
@@ -47,85 +51,20 @@ extension TransPathRepository {
     
     // TransPathDTO -> MakchaInfo 변환 메서드
     func convertTransPathDTOToMakchaInfo(transPathDTO: TransPathDTO) -> MakchaInfo? {
-        let pathArr = transPathDTO.path
-        guard let firstPath = pathArr.first else { return nil }
-        let startStation = firstPath.info.firstStartStation // 전체 경로 중 출발역
-        let endStation = firstPath.info.lastEndStation // 전체 경로 중 도착역
+        let pathArr: [Path] = transPathDTO.result.path
         
         var makchaPathArr = [MakchaPath]()
         for i in 0..<pathArr.count {
             // 경로 유형 구하기
-            var makchaPathType: MakchaPathType
-            switch pathArr[i].pathType {
-            case 1:
-                makchaPathType = .subway // 지하철
-            case 2:
-                makchaPathType = .bus // 버스
-            case 3:
-                makchaPathType = .both // 버스+지하철
-            default:
+            guard let makchaPathType: MakchaPathType = convertIntToMakchaPathType(makchaPathTypeInt: pathArr[i].pathType) else {
                 return nil
             }
-            
             // 총 소요시간 구하기
             let totalTime = pathArr[i].info.totalTime
             
             // 세부경로 구하기
-            let subPathArr = pathArr[i].subPath
-            var makchaSubPathArr = [MakchaSubPath]()
-            for j in 0..<subPathArr.count {
-                // 세부경로 유형 구하기
-                var subPathType: SubPathType
-                switch subPathArr[j].trafficType {
-                case 1:
-                    subPathType = .subway // 지하철
-                case 2:
-                    subPathType = .bus // 버스
-                case 3:
-                    subPathType = .walk // 도보
-                default:
-                    return nil
-                }
-                
-                // 세부경로 교통수단 정보
-                var laneInfoArr = [LaneInfo]()
-                if let laneArr = subPathArr[j].lane {
-                    for eachLane in laneArr {
-                        let laneInfo: LaneInfo
-                        if let subwayLine = eachLane.name {
-                            laneInfo = LaneInfo(name: subwayLine)
-                            laneInfoArr.append(laneInfo)
-                            continue
-                        }
-                        if let busNo = eachLane.busNo {
-                            laneInfo = LaneInfo(name: busNo)
-                            laneInfoArr.append(laneInfo)
-                            continue
-                        }
-                    }
-                }
-                
-                // 지나는 역
-                var passStationArr = [PassStation]()
-                if let stationArr = subPathArr[j].passStopList?.stations {
-                    for station in stationArr {
-                        let passStation = PassStation(index: station.index, name: station.stationName)
-                        passStationArr.append(passStation)
-                    }
-                }
-                
-                let makchaSubPath = MakchaSubPath(
-                    subPathType: subPathType,
-                    distance: subPathArr[j].distance,
-                    time: subPathArr[j].sectionTime,
-                    stationCount: subPathArr[j].stationCount,
-                    lane: laneInfoArr.isEmpty ? nil : laneInfoArr,
-                    startName: subPathArr[j].startName,
-                    endName: subPathArr[j].endName,
-                    stations: passStationArr.isEmpty ? nil : passStationArr
-                )
-                
-                makchaSubPathArr.append(makchaSubPath)
+            guard let makchaSubPathArr = makeSubPathArr(subPathArr: pathArr[i].subPath) else {
+                return nil
             }
             
             let makchaPath = MakchaPath(
@@ -138,11 +77,98 @@ extension TransPathRepository {
         }
         
         let makchaInfo = MakchaInfo(
-            start: startStation,
-            end: endStation,
             makchaPaths: makchaPathArr
         )
         
         return makchaInfo
+    }
+    
+    func convertIntToMakchaPathType(makchaPathTypeInt: Int) -> MakchaPathType? {
+        switch makchaPathTypeInt {
+        case 1:
+            return .subway
+        case 2:
+            return .bus
+        case 3:
+            return .both
+        default:
+            return nil
+        }
+    }
+    
+    func makeSubPathArr(subPathArr: [SubPath]) -> [MakchaSubPath]? {
+        var makchaSubPathArr = [MakchaSubPath]()
+        for j in 0..<subPathArr.count {
+            // 세부경로 [유형] 구하기
+            guard let subPathType: SubPathType = convertIntToSubPathType(subPathTypeInt: subPathArr[j].trafficType) else {
+                return nil
+            }
+            // 세부경로 [교통수단 정보] 구하기
+            let laneInfoArr: [LaneInfo] = makeLaneInfoArr(lane: subPathArr[j].lane)
+            // 세부경로 [지나는 역] 구하기
+            let passStationArr: [PassStation] = makePassStaionArr(passStopList: subPathArr[j].passStopList)
+            
+            let makchaSubPath = MakchaSubPath(
+                subPathType: subPathType,
+                distance: subPathArr[j].distance,
+                time: subPathArr[j].sectionTime,
+                stationCount: subPathArr[j].stationCount,
+                lane: laneInfoArr.isEmpty ? nil : laneInfoArr,
+                startName: subPathArr[j].startName,
+                endName: subPathArr[j].endName,
+                stations: passStationArr.isEmpty ? nil : passStationArr
+            )
+            
+            makchaSubPathArr.append(makchaSubPath)
+        }
+        return makchaSubPathArr
+    }
+    
+    func convertIntToSubPathType(subPathTypeInt: Int) -> SubPathType? {
+        switch subPathTypeInt {
+        case 1:
+            return .subway // 지하철
+        case 2:
+            return .bus // 버스
+        case 3:
+            return .walk // 도보
+        default:
+            return nil
+        }
+    }
+    
+    func makeLaneInfoArr(lane: [Lane]?) -> [LaneInfo] {
+        if let laneArr = lane {
+            var laneInfoArr = [LaneInfo]()
+            for eachLane in laneArr {
+                let laneInfo: LaneInfo
+                if let subwayLine = eachLane.name {
+                    laneInfo = LaneInfo(name: subwayLine)
+                    laneInfoArr.append(laneInfo)
+                    continue
+                }
+                if let busNo = eachLane.busNo {
+                    laneInfo = LaneInfo(name: busNo)
+                    laneInfoArr.append(laneInfo)
+                    continue
+                }
+            }
+            return laneInfoArr
+        } else {
+            return []
+        }
+    }
+    
+    func makePassStaionArr(passStopList: PassStopList?) -> [PassStation] {
+        if let stationArr = passStopList?.stations {
+            var passStationArr = [PassStation]()
+            for station in stationArr {
+                let passStation = PassStation(index: station.index, name: station.stationName)
+                passStationArr.append(passStation)
+            }
+            return passStationArr
+        } else {
+            return []
+        }
     }
 }
