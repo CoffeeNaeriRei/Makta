a7d229df2f572a3193ed9cfb7724b21101acfd3f
kybeen <rei980627@gmail.com>
2024-05-09 14:43:47 +0900

    [feat] #20 - MakchaPathUseCase.swift 파일 생성

---
+++ Makcha/Makcha/Sources/Domain/UseCase/MakchaPathUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  MakchaPathUseCase.swift
+//  Makcha
+//
+//  Created by 김영빈 on 5/9/24.
+//
+
+import Foundation
+
+// MARK: - 막차 경로 불러오기 관련 유즈케이스
+
+final class MakchaPathUseCase {
+    
+}
