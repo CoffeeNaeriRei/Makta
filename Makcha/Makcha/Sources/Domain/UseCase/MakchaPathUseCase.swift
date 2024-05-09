bdee265cefefe8d4828a3d2b6dd719792241ac05
kybeen <rei980627@gmail.com>
2024-05-09 19:12:27 +0900

    [feat] #20 - MakchaPathUseCase 메서드 구현 - 막차 경로 검색 메서드 구현 - 현재 위치 기반으로 막차 경로 불러오는 메서드 구현

--- Makcha/Makcha/Sources/Domain/UseCase/MakchaPathUseCase.swift
+++ Makcha/Makcha/Sources/Domain/UseCase/MakchaPathUseCase.swift
@@ -7,8 +7,60 @@
 
 import Foundation
 
+import RxCocoa
+import RxRelay
+import RxSwift
+
 // MARK: - 막차 경로 불러오기 관련 유즈케이스
 
 final class MakchaPathUseCase {
     
+    private let transPathRepository: TransPathRepositoryProtocol
+    
+    let makchaInfo = PublishSubject<MakchaInfo>() // 막차 정보
+    let startPoint = PublishSubject<EndPoint>() // 출발지 정보
+    let destinationPoint: BehaviorRelay<EndPoint> // 도착지 정보
+    
+    private let disposeBag = DisposeBag()
+    
+    init(_ transPathRepository: TransPathRepositoryProtocol) {
+        self.transPathRepository = transPathRepository
+        
+        // TODO: 사용자가 설정한 기본 목적지로 초기화하기
+        let destionationCoordinate = mockDestinationPoint
+        destinationPoint = BehaviorRelay<EndPoint>(value: destionationCoordinate)
+    }
+    
+    func updateStartPointToSearchedLocation() {
+        
+    }
+    
+    func updateDestinationPointToSearchedLocation() {
+        
+    }
+    
+    // 막차 경로 검색
+    func loadMakchaPath(start: XYCoordinate, end: XYCoordinate) {
+        transPathRepository.getAllMakchaTransPath(start: start, end: end)
+            .bind(to: makchaInfo)
+            .disposed(by: disposeBag)
+    }
+    
+    // 현재 위치 기반으로 막차 경로 불러오기
+    func loadMakchaPathWithCurrentLocation() {
+        transPathRepository.getCurrentLocation()
+            .do(
+                onNext: { [weak self] currentLocation in
+                    guard let destinationCoordinate = self?.destinationPoint.value.coordinate else { return }
+                    self?.loadMakchaPath(start: currentLocation.coordinate, end: destinationCoordinate)
+                }
+            )
+            .bind(to: startPoint)
+            .disposed(by: disposeBag)
+    }
+    
+    // 검색한 위치 기반으로 막차 경로 불러오기
+    func loadMakchaPathWithSearchedLocation() {
+        
+    }
 }
