f1bd53b8392d14b96e3674b43f8585392247c5ea
kybeen <rei980627@gmail.com>
2024-05-10 23:29:35 +0900

    [test] #20 - EndPointRepository 리버스 지오코딩 관련 테스트 코드 작성

--- Makcha/MakchaTests/Data/Repository/EndPointRepositoryTests.swift
+++ Makcha/MakchaTests/Data/Repository/EndPointRepositoryTests.swift
@@ -16,11 +16,28 @@ import RxTest
 // Mock LocationService
 struct MockLocationService: LocationServiceInterface {
     
-    var mockCLLocation: CLLocation?
+    var mockEndPoint: EndPoint?
+    var isConvertSuccess: Bool = true
     var mockError: Error?
     
     func fetchCurrentLocation(completion: @escaping LocationCallback) {
+        let mockCLLocation = CLLocation(
+            latitude: CLLocationDegrees(mockEndPoint!.coordinate.latY)!,
+            longitude: CLLocationDegrees(mockEndPoint!.coordinate.lonX)!
+        )
         completion(mockCLLocation, mockError)
+    }
+    
+    func convertCoordinateToAddress(
+        lon: CLLocationDegrees,
+        lat: CLLocationDegrees,
+        completion: @escaping ((String?) -> Void)
+    ) {
+        if isConvertSuccess {
+            completion(mockEndPoint?.name)
+        } else {
+            completion(nil)
+        }
     }
 }
 
@@ -46,14 +63,11 @@ final class EndPointRepositoryTests: XCTestCase {
     
     func test_위치데이터를_정상적으로_받아왔을때_getCurrentLocation이_정상적으로_이벤트를_방출하는지_확인() {
         // Given
-        let lat = CLLocationDegrees(mockStartPoint.coordinate.latY)
-        let lon = CLLocationDegrees(mockStartPoint.coordinate.lonX)
-        let mockCLLocation: CLLocation = CLLocation(latitude: lat!, longitude: lon!)
-        let expectedEndPoint: EndPoint = EndPoint(
-            coordinate: (mockCLLocation.coordinate.longitude.description, mockCLLocation.coordinate.latitude.description)
-        )
+        let mockCurrentLocation = mockStartPoint
+        let expectedName = mockStartPoint.name
+        let expectedEndPoint = mockStartPoint
         var resultEndPoint: EndPoint
-        sut = EndPointRepository(MockLocationService(mockCLLocation: mockCLLocation))
+        sut = EndPointRepository(MockLocationService(mockEndPoint: mockCurrentLocation, isConvertSuccess: true))
         let endPointObserver = scheduler.createObserver(EndPoint.self)
 
         // When
@@ -64,6 +78,33 @@ final class EndPointRepositoryTests: XCTestCase {
         // Then
         XCTAssertEqual(endPointObserver.events.count, 2) // 이벤트 2개(onNext,onCompleted)가 호출되는지 확인
         resultEndPoint = endPointObserver.events.first!.value.element!
+        XCTAssertEqual(expectedEndPoint, resultEndPoint)
+        XCTAssertEqual(resultEndPoint.name, expectedName)
+    }
+    
+    func test_받아온_위치데이터의_리버스지오코딩이_실패했을때_getCurrentLocation_예외처리를_제대로하는지_확인() {
+        // Given
+        let mockCurrentLocation = mockStartPoint
+        let mockLatStr = mockCurrentLocation.coordinate.latY.description
+        let mockLonStr = mockCurrentLocation.coordinate.lonX.description
+        let expectedName = "\(mockLatStr),\(mockLonStr) (좌표변환 실패)"
+        let expectedEndPoint = EndPoint(
+            name: expectedName,
+            coordinate: (mockLonStr, mockLatStr)
+        )
+        var resultEndPoint: EndPoint
+        sut = EndPointRepository(MockLocationService(mockEndPoint: mockCurrentLocation, isConvertSuccess: false))
+        let endPointObserver = scheduler.createObserver(EndPoint.self)
+
+        // When
+        sut.getCurrentLocation()
+            .bind(to: endPointObserver)
+            .disposed(by: disposeBag)
+
+        // Then
+        XCTAssertEqual(endPointObserver.events.count, 2) // 이벤트 2개(onNext,onCompleted)가 호출되는지 확인
+        resultEndPoint = endPointObserver.events.first!.value.element!
+        XCTAssertEqual(expectedName, resultEndPoint.name)
         XCTAssertEqual(expectedEndPoint, resultEndPoint)
     }
 }
