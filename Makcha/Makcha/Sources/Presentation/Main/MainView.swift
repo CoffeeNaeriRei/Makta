caee7ea3c982b736cb5809fe1a55e89a8b88d4a6
yuncoffee <dong072815@gmail.com>
2024-05-11 23:56:03 +0900

    [test] MainView Layout 테스트 #6

--- Makcha/Makcha/Sources/Presentation/Main/MainView.swift
+++ Makcha/Makcha/Sources/Presentation/Main/MainView.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 import MakchaDesignSystem
 import PinLayout
 import FlexLayout
@@ -14,13 +15,7 @@ import FlexLayout
 final class MainView: UIView {
     private var rootView = UIView()
     
-    var button1: UIButton {
-        let button = UIButton()
-        
-        button.setTitle("Hello", for: .normal)
-        
-        return button
-    }
+    let button1 = UIButton()
     
     init() {
         super.init(frame: .zero)
@@ -35,22 +30,58 @@ final class MainView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        rootView.pin.all()
+        rootView.pin.all(pin.safeArea)
         rootView.flex.layout()
     }
     
     private func layout() {
-        rootView.flex.define {
+        button1.setTitle("Hello", for: .normal)        
+        rootView.flex.gap(.cfSpacing(.large)).define {
             $0.addItem(button1)
                 .width(200).height(80)
                 .backgroundColor(.blue)
+            $0.addItem().direction(.row)
+                .gap(.cfSpacing(.large))
+                .padding(.cfSpacing(.large), .cfSpacing(.medium))
+                .define {
+                    $0.addItem()
+                        .direction(.row)
+                        .padding(12)
+                        .justifyContent(.center)
+                        .gap(.cfSpacing(.xxxlarge)).define {
+                        $0.addItem()
+                            .backgroundColor(UIColor(Color.transport(.subway(.metropolitanRailway(.line2)))))
+                            .height(100%)
+                            .grow(1)
+                        $0.addItem()
+                            .backgroundColor(UIColor(Color.transport(.subway(.metropolitanRailway(.line3)))))
+                            .height(100%)
+                            .grow(1)
+                    }
+                    .backgroundColor(UIColor(Color.transport(.bus(.gyeonggiBusType(.마을)))))
+                    .height(100%).grow(1)
+                $0.addItem()
+                    .backgroundColor(UIColor(Color.transport(.subway(.metropolitanRailway(.line2)))))
+                    .height(100%).grow(1)
+                $0.addItem()
+                    .backgroundColor(UIColor(Color.transport(.subway(.metropolitanRailway(.line3)))))
+                    .height(100%).grow(1)
+                $0.addItem()
+                    .backgroundColor(UIColor(Color.transport(.subway(.metropolitanRailway(.line4)))))
+                    .height(100%).grow(1)
+            }
+            .height(80)
+            .border(1, .blue)
+            
+            $0.addItem()
+                .width(400).height(200)
+                .backgroundColor(UIColor(Color.transport(.subway(.metropolitanRailway(.line1)))))
         }
         .border(1, .red)
     }
 }
 
 #if DEBUG
-import SwiftUI
 struct MainView_Preview: PreviewProvider {
     static var previews: some View {
         ViewPreview {
--- Makcha/Makcha/Sources/Presentation/Main/MainViewController.swift
+++ Makcha/Makcha/Sources/Presentation/Main/MainViewController.swift
@@ -8,19 +8,37 @@
 import Foundation
 import UIKit
 import RxSwift
+import RxCocoa
 
 final class MainViewController: UIViewController {
-    public let testView = TestView()
+    // swiftlint: disable force_cast
+    var mainView: MainView {
+        view as! MainView
+    }
+    // swiftlint: enable force_cast
     
     let disposeBag = DisposeBag()
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        print("ViewDidLoaded")
+        bind()
     }
-
+    
     public override func loadView() {
-        view = testView
+        view = MainView()
+    }
+    
+    private func bind() {
+        mainView.button1.rx.tap
+            .withUnretained(self)
+            .subscribe { vc, _ in
+                vc.buttonAction()
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func buttonAction() {
+        print("HELLO WORLD!")
     }
 }
 
