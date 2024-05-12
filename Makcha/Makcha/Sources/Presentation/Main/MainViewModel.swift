0c81c5198427600f1190e9638a2d8d79a417f9b3
kybeen <rei980627@gmail.com>
2024-05-12 13:27:58 +0900

    merge develop

---
+++ Makcha/Makcha/Sources/Presentation/Main/MainViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewModel.swift
+//  Makcha
+//
+//  Created by 김영빈 on 5/10/24.
+//
+
+// MARK: - 메인 화면의 뷰모델 (막차 경로 불러오는 화면)
+
+protocol MainViewModelType {
+    // input
+    
+    // output
+}
+
+final class MainViewModel: MainViewModelType {
+    
+}
