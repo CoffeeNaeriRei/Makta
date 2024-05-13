10db6536cad37dc22c603fedf47c1d0db65377df
kybeen <rei980627@gmail.com>
2024-05-13 15:10:51 +0900

    [feat] #27 - APIService에 서울시 실시간 지하철 도착 정보 API 호출 코드 작성

--- Makcha/Makcha/Sources/Data/External/APIService/APIService.swift
+++ Makcha/Makcha/Sources/Data/External/APIService/APIService.swift
@@ -23,37 +23,66 @@ protocol APIServiceInterface {
 
 struct APIService: APIServiceInterface {
     
-    // API 요청
+    // 대중교통 환승경로 API 요청
     func fetchTransPathData(
         start: XYCoordinate,
         end: XYCoordinate,
-        completion: @escaping (Result<TransPathDTO, APIServiceError>) -> Void) {
-            guard let transPathURL = makeTransPathURL(start: start, end: end),
-                  let url = URL(string: transPathURL) else {
-                completion(.failure(APIServiceError.invalidURL))
+        completion: @escaping (Result<TransPathDTO, APIServiceError>) -> Void
+    ) {
+        guard let transPathURL = makeTransPathURL(start: start, end: end),
+              let url = URL(string: transPathURL) else {
+            completion(.failure(APIServiceError.invalidURL))
+            return
+        }
+            
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if error != nil {
+                completion(.failure(APIServiceError.requestFail))
+            }
+            guard let data = data else {
+                completion(.failure(APIServiceError.noData))
                 return
             }
-            
-            URLSession.shared.dataTask(with: url) { (data, _, error) in
-                if error != nil {
-                    completion(.failure(APIServiceError.requestFail))
-                }
-                guard let data = data else {
-                    completion(.failure(APIServiceError.noData))
-                    return
-                }
-                guard let transPathDTO = try? JSONDecoder().decode(TransPathDTO.self, from: data) else {
-                    completion(.failure(APIServiceError.decodingError))
-                    return
-                }
-                completion(.success(transPathDTO))
-            }.resume()
+            guard let transPathDTO = try? JSONDecoder().decode(TransPathDTO.self, from: data) else {
+                completion(.failure(APIServiceError.decodingError))
+                return
+            }
+            completion(.success(transPathDTO))
+        }.resume()
+    }
+    
+    // 서울시 실시간 지하철 도착 정보 API 요청
+    func fetchSeoulRealtimeSubwayArrival(
+        stationName: String,
+        completion: @escaping (Result<SeoulRealtimeSubwayDTO, APIServiceError>) -> Void
+    ) {
+        guard let seoulRealtimeSubwayURL = makeSeoulRealtimeSubwayURL(stationName: stationName),
+              let url = URL(string: seoulRealtimeSubwayURL) else {
+            completion(.failure(APIServiceError.invalidURL))
+            return
+        }
+        print("서울URL: \(seoulRealtimeSubwayURL)")
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if error != nil {
+                completion(.failure(APIServiceError.requestFail))
+            }
+            guard let data = data else {
+                completion(.failure(APIServiceError.noData))
+                return
+            }
+            guard let seoulRealtimeSubwayDTO = try? JSONDecoder().decode(SeoulRealtimeSubwayDTO.self, from: data) else {
+                completion(.failure(APIServiceError.decodingError))
+                return
+            }
+            completion(.success(seoulRealtimeSubwayDTO))
+        }.resume()
     }
 }
 
 extension APIService {
     
-    // API 요청 URL 생성
+    // [대중교통 환승경로 API] 요청 URL 생성
     func makeTransPathURL(
         start: XYCoordinate,
         end: XYCoordinate,
@@ -74,5 +103,22 @@ extension APIService {
         transPathURL += "&SearchPathType=\(searchPathType.rawValue)"
         
         return transPathURL
+    }
+    
+    // [서울시 실시간 지하철 도착 정보 API] 요청 URL 생성
+    func makeSeoulRealtimeSubwayURL(
+        stationName: String,
+        startIdx: Int = 0, // 페이징 시작번호
+        endIdx: Int = 15 // 페이징 끝번호
+    ) -> String? {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "REALTIME_STATION_API") as? String else {
+            return nil
+        }
+        var seoulRealtimeSubwayURL = "http://swopenAPI.seoul.go.kr/api/subway/\(apiKey)/json/realtimeStationArrival"
+        seoulRealtimeSubwayURL += "/\(startIdx)"
+        seoulRealtimeSubwayURL += "/\(endIdx)"
+        seoulRealtimeSubwayURL += "/\(stationName)"
+        
+        return seoulRealtimeSubwayURL
     }
 }
