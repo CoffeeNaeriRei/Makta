0bd9d3c56013bb0357a17d95a84555c1414c9272
yuncoffee <dong072815@gmail.com>
2024-05-22 23:06:03 +0900

    [feat] MainCollectionViewCell 레이아웃 작성 #12

--- Makcha/Makcha/Sources/Presentation/Main/MainCollectionView/MainCollectionCell.swift
+++ Makcha/Makcha/Sources/Presentation/Main/MainCollectionView/MainCollectionCell.swift
@@ -12,7 +12,7 @@ import FlexLayout
 import MakchaDesignSystem
 
 final class MainCollectionCell: UICollectionViewCell {
-    private var pathTypelabel: UILabel {
+    var pathTypelabel: UILabel {
         let label = UILabel()
         label.attributedText = .pretendard("경로 종류",
                                            scale: .body,
@@ -41,12 +41,12 @@ final class MainCollectionCell: UICollectionViewCell {
     private var navigationToDetailsButton: UIButton {
         let button = UIButton()
         
-        var tintColor = UIColor(Color.cf(.grayScale(.gray800)))
+        let tintColor = UIColor(Color.cf(.grayScale(.gray800)))
         let symbolConfig = UIImage.SymbolConfiguration(pointSize: 14,
                                                        weight: .regular,
                                                        scale: .default)
         
-        var icon = UIImage(systemName: "chevron.right",
+        let icon = UIImage(systemName: "chevron.right",
                            withConfiguration: symbolConfig)?
             .withTintColor(tintColor, renderingMode: .alwaysOriginal)
         var titleAttr = AttributedString("자세히 보기")
@@ -68,6 +68,37 @@ final class MainCollectionCell: UICollectionViewCell {
         return button
     }
     
+    // MARK: 현재 도착 예정인 교통수단이 정거장에 도착하기 까지 남은 시간을 표시하는 라벨
+    var currentArrivalTransportTimeLabel: UILabel {
+        let label = UILabel()
+        let textColor = UIColor(Color.cf(.grayScale(.black)))
+        
+        label.attributedText = .pretendard("NN분\n NN초", scale: .display)
+        label.textColor = textColor
+        
+        return label
+    }
+
+    // MARK: 다음 도착 예정인 교통수단이 정거장에 도착하기 까지 남은 시간을 표시하는 라벨
+    var nextArrivalTransportTimeLabel: UILabel {
+        let label = UILabel()
+        let textColor = UIColor(Color.cf(.grayScale(.gray500)))
+        
+        label.attributedText = .pretendard("다음 도착 NN분 예정", scale: .body)
+        label.textColor = textColor
+        
+        return label
+    }
+    
+    private var centerContentsTopLabel: UILabel {
+        let label = UILabel()
+        let textColor = UIColor(Color.cf(.grayScale(.gray600)))
+        label.attributedText = .pretendard("도착 예정", scale: .caption)
+        label.textColor = textColor
+        
+        return label
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
@@ -79,6 +110,7 @@ final class MainCollectionCell: UICollectionViewCell {
     
     private func setup() {
         contentView.flex.define {
+            /// TopContents
             $0.addItem().direction(.row).alignItems(.start).define {
                 $0.addItem().justifyContent(.spaceBetween).define {
                     $0.addItem()
@@ -92,18 +124,46 @@ final class MainCollectionCell: UICollectionViewCell {
                     $0.addItem(durationTimeLabel)
                 }
                 .grow(1)
-                .paddingTop(.cfSpacing(.large))
-                .paddingLeft(.cfSpacing(.xxlarge))
+                .paddingTop(.cfSpacing(.xlarge))
+                .paddingLeft(.cfSpacing(.xxxlarge))
                 $0.addItem(navigationToDetailsButton)
                     .marginTop(5)
             }
             .grow(1)
+            
+            /// CenterContents
+            $0.addItem().alignItems(.center).alignSelf(.center).define {
+                $0.addItem(centerContentsTopLabel)
+                $0.addItem(currentArrivalTransportTimeLabel)
+                $0.addItem(nextArrivalTransportTimeLabel)
+            }
+            .position(.absolute).top(64)
+            
+            /// BottomContents
+            $0.addItem(layoutPathInfo())
+                .width(100%)
+                .position(.absolute).bottom(0)
+                
+            /// BottomLine
             $0.addItem()
-                .width(100%).height(.cfStroke(.small))
+                .width(100%).height(.cfStroke(.xsmall))
                 .backgroundColor(UIColor(Color.cf(.grayScale(.gray200))))
         }
         .backgroundColor(UIColor(Color.cf(.grayScale(.white))))
-        .minHeight(240)
+        .height(240)
+    }
+    
+    func layoutPathInfo() -> UIView {
+        let rootView = UIView()
+        
+        rootView.flex.define {
+            $0.addItem()
+        }
+        .minHeight(66)
+        .marginBottom(12)
+        .border(1, .red)
+        
+        return rootView
     }
     
     private func layout() {
