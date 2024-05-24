614dd2a2d9ff463d29d04c14a0125ae48038c20e
kybeen <rei980627@gmail.com>
2024-05-24 21:30:17 +0900

    [feat] #27 - ArrivalStatus 타입 case 별로 우선순위 비교 가능하도록 Comparable 프로토콜 채택

--- Makcha/Makcha/Sources/Domain/Entity/ArrivalType.swift
+++ Makcha/Makcha/Sources/Domain/Entity/ArrivalType.swift
@@ -11,10 +11,11 @@
 typealias RealtimeArrivalTuple = (first: ArrivalStatus, second: ArrivalStatus)
 
 // 버스와 지하철의 실시간 도착 상태를 나타내는 타입
-enum ArrivalStatus {
+/// Comparable 프로토콜을 채택해서 표시할 때의 우선순위를 비교할 수 있도록 함 (위로 갈수록 우선순위가 더 큼 + coming의 경우 적은 시간이 남을수록 우선순위가 높음)
+enum ArrivalStatus: Comparable {
+    case arriveSoon // 곧 도착
+    case coming(remainingSecond: Int) // 운행중(남은시간(초))
     case waiting // 출발대기
     case finished // 운행종료
-    case arriveSoon // 곧 도착
-    case coming(remaingSecond: Int) // 운행중(남은시간(초))
     case unknown // 알 수 없음
 }
