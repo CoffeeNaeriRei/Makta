3fa6cd2f4aeea34938b4f8bea88dae9115815c97
kybeen <rei980627@gmail.com>
2024-06-01 02:10:21 +0900

    [feat] #27 - 실시간 도착 정보 타이머 구현

--- Makcha/Makcha/Sources/Domain/UseCase/MakchaInfoUseCase.swift
+++ Makcha/Makcha/Sources/Domain/UseCase/MakchaInfoUseCase.swift
@@ -21,8 +21,10 @@ final class MakchaInfoUseCase {
     var realtimeArrivals = PublishSubject<[RealtimeArrivalTuple]>() // 막차 경로 별 실시간 도착 정보
     let startPoint = BehaviorRelay<EndPoint>(value: mockStartPoint) // 출발지 정보 // TODO: - 기본값 지정하기
     let destinationPoint: BehaviorRelay<EndPoint> // 도착지 정보
+    let timerEvent = PublishRelay<Void>() // 실시간 도착 정보 타이머 이벤트
     
     private let disposeBag = DisposeBag()
+    private var timerDisposable: Disposable? // 타이머 구독을 제어하기 위한 Disposable
     
     init(_ transPathRepository: TransPathRepositoryProtocol, _ endPointRepository: EndPointRepositoryProtocol) {
         self.transPathRepository = transPathRepository
@@ -31,6 +33,26 @@ final class MakchaInfoUseCase {
         // TODO: 사용자가 설정한 기본 목적지로 초기화하기
         let destionationCoordinate = mockDestinationPoint
         destinationPoint = BehaviorRelay<EndPoint>(value: destionationCoordinate)
+        
+        bind()
+    }
+    
+    func bind() {
+        // 타이머 이벤트 구독
+        timerEvent
+            .withUnretained(self)
+            .withLatestFrom(realtimeArrivals)
+            .map { prevRealtimeArrivals in
+                prevRealtimeArrivals.map { arrival in
+                    (
+                        first: self.decreaseSecondFromArrivalStatus(arrival.first),
+                        second: self.decreaseSecondFromArrivalStatus(arrival.second)
+                    )
+                }
+            }
+            .debug()
+            .bind(to: realtimeArrivals)
+            .disposed(by: disposeBag)
     }
     
     func updateStartPointToSearchedLocation() {
@@ -41,7 +63,7 @@ final class MakchaInfoUseCase {
         
     }
     
-    // 막차 경로 검색
+    // MARK: - 막차 경로 검색
     func loadMakchaPath(start: XYCoordinate, end: XYCoordinate) {
         transPathRepository.getAllMakchaTransPath(start: start, end: end)
             .withUnretained(self)
@@ -54,7 +76,7 @@ final class MakchaInfoUseCase {
             .disposed(by: disposeBag)
     }
     
-    // 현재 위치 기반으로 막차 경로 불러오기
+    // MARK: - 현재 위치 기반으로 막차 경로 불러오기
     func loadMakchaPathWithCurrentLocation() {
         print("[MakchaInfoUseCase] - 현재 위치 기반으로 막차 경로 불러오기")
         endPointRepository.getCurrentLocation()
@@ -68,14 +90,14 @@ final class MakchaInfoUseCase {
             .disposed(by: disposeBag)
     }
     
-    // 검색한 위치 기반으로 막차 경로 불러오기
+    // MARK: - 검색한 위치 기반으로 막차 경로 불러오기
     func loadMakchaPathWithSearchedLocation() {
         let start = startPoint.value.coordinate
         let end = destinationPoint.value.coordinate
         loadMakchaPath(start: start, end: end)
     }
     
-    // MakchaPath 배열을 받아와서 각 경로별 실시간 도착정보를 만들어주는 메서드
+    // MARK: - MakchaPath 배열을 받아와서 각 경로별 실시간 도착정보를 만들어주는 메서드
     func makeRealtimeArrivalTimes(currentTime: Date, makchaPaths: [MakchaPath]) {
         var realtimeArrivalObservables: [Observable<RealtimeArrivalTuple>] = []
         
@@ -120,12 +142,48 @@ final class MakchaInfoUseCase {
             }
         }
         
-        // TODO: - combineLatest 동작 원리 제대로 모름. 다른 방법도 있는지 생각해보기
         Observable.combineLatest(realtimeArrivalObservables)
 //            .debug()
-            .subscribe(onNext: { [weak self] arrivals in
-                self?.realtimeArrivals.onNext(arrivals)
+            .withUnretained(self)
+            .subscribe(onNext: { _, arrivals in
+                self.realtimeArrivals.onNext(arrivals)
+                self.startTimer()
             })
             .disposed(by: disposeBag)
+    }
+    
+    // MARK: - 타이머 시작
+    private func startTimer() {
+        print("타이머 시작")
+        timerDisposable?.dispose() // 기존 타이머 종료
+        timerDisposable = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+//            .take(몇초까지할지)
+            .withUnretained(self)
+            .subscribe(onNext: { _ in
+                self.timerEvent.accept(())
+            }, onCompleted: {
+                print("타이머 종료")
+            })
+        
+        timerDisposable?.disposed(by: disposeBag)
+        
+        // TODO: - 새 타이머가 시작될 때 외에도 동작 중인 타이머를 종료시킬 시점이 더 필요한지 생각해보기
+    }
+
+    // MARK: - ArrivalStatus의 남은 시간 카운트
+    /// ArrivalStatus가 .coming일 경우 남은시간(초)를 1 줄여줌
+    private func decreaseSecondFromArrivalStatus(_ arrivalStatus: ArrivalStatus) -> ArrivalStatus {
+        switch arrivalStatus {
+        case .coming(let remainingSecond):
+            if remainingSecond == 0 {
+                return .coming(remainingSecond: 0)
+            } else if remainingSecond > 0 {
+                return .coming(remainingSecond: remainingSecond - 1)
+            } else {
+                return .unknown
+            }
+        default:
+            return arrivalStatus
+        }
     }
 }
