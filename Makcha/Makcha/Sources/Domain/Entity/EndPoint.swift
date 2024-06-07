5a9d2f36b61c7043549f2839887e924aaf815977
kybeen <rei980627@gmail.com>
2024-06-08 02:04:43 +0900

    [fix] #36 - EndPoint 구성 수정 (주소 구분, 장소이름 관련 프로퍼티 추가)

--- Makcha/Makcha/Sources/Domain/Entity/EndPoint.swift
+++ Makcha/Makcha/Sources/Domain/Entity/EndPoint.swift
@@ -9,22 +9,27 @@
 // 막차 경로 검색의 기준이 되는 출발점, 도착점의 좌표
 
 struct EndPoint: Equatable {
-    let name: String // 좌표 주소명
+    let name: String // 장소 이름
+    let addressName: String // 지번주소
+    let roadAddressName: String? // 도로명주소
     let coordinate: XYCoordinate // 좌표
     
     static func == (lhs: EndPoint, rhs: EndPoint) -> Bool {
-        return lhs.coordinate == rhs.coordinate
+        return (lhs.name == rhs.name) || (lhs.addressName == rhs.addressName) || (lhs.roadAddressName == rhs.roadAddressName) || (lhs.coordinate == rhs.coordinate)
     }
 }
 
 #if DEBUG
 let mockStartPoint = EndPoint(
-//    name: "서울특별시 은평구 대조동 2-44",
-    name: "서울특별시 대조동 대조동 2-44",
-    coordinate: ("126.926493082645", "37.6134436427887")
+    name: "대조어린이공원",
+    addressName: "서울 은평구 대조동 212",
+    roadAddressName: "서울 은평구 연서로22길 9-30",
+    coordinate: ("126.919971934734", "37.6155372675701")
 )
 let mockDestinationPoint = EndPoint(
-    name: "서울특별시 송파구 중대로 149",
-    coordinate: ("127.126936754911", "37.5004198786564")
+    name: "경찰병원역 3호선",
+    addressName: "서울 송파구 가락동 10-15",
+    roadAddressName: "서울 송파구 중대로 지하 149",
+    coordinate: ("127.124482397777", "37.4960049150853")
 )
 #endif
