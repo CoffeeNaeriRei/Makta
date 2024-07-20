643f32cea7f4e05676df8d15af2778473555a1a7
kybeen <rei980627@gmail.com>
2024-07-20 20:03:58 +0900

    [fix] #46 - 도착 정보 시간 3자리로 표시되는 문제 수정

--- Makcha/Makcha/Sources/Utils/Int+Extension.swift
+++ Makcha/Makcha/Sources/Utils/Int+Extension.swift
@@ -10,6 +10,7 @@ import Foundation
 extension Int {
     // Int 단위의 초 값을 00분00초 형식의 String으로 변환하는 프로퍼티
     var convertToMinuteSecondString: String {
+        guard self >= 0 else { return "도착 정보 없음" }
         var minuteStr = String(self / 60)
         var secondStr = String(self % 60)
         var result = ""
@@ -20,7 +21,7 @@ extension Int {
         result += "\(minuteStr)분 "
         
         if secondStr.count == 1 {
-            secondStr = "0\(minuteStr)"
+            secondStr = "0\(secondStr)"
         }
         result += "\(secondStr)초"
         
